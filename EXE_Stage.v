aa62a1008b06448484598f2aef523424a0ee5db9
eliza1378 <73926711+eliza1378@users.noreply.github.com>
2020-11-06 12:33:46 +0330

    Update EXE_Stage.v

--- EXE_Stage.v
+++ EXE_Stage.v
@@ -2,10 +2,10 @@
 
 module EXE_Stage
 (
-    input                       clk,
-    input                       rst,
-    input      [`WORD_WIDTH-1:0] pc_in,
-    input      [`WORD_WIDTH-1:0] instruction_in,
+    input                    clk,
+    input                    rst,
+    input  [`WORD_WIDTH-1:0] pc_in,
+    input  [`WORD_WIDTH-1:0] instruction_in,
     output [`WORD_WIDTH-1:0] pc,
     output [`WORD_WIDTH-1:0] instruction
 );
@@ -14,6 +14,3 @@ assign pc = pc_in;
 assign instruction = instruction_in;
 
 endmodule
-
-
-
