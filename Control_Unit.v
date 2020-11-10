2e0375b3f636a0ee711525d64eb05616ca5077a5
eliza1378 <73926711+eliza1378@users.noreply.github.com>
2020-11-11 02:02:29 +0330

    Update Control_Unit.v

--- Control_Unit.v
+++ Control_Unit.v
@@ -2,39 +2,35 @@
 
 module Control_Unit
 (
-    input        S, I,
-    input  [1:0] mode,
-    input  [3:0] op_code,
-    output [3:0] EX_command,
-    output       mem_read,
-    output       mem_write,
-    output       WB_en,
-    output       Imm,
-    output       B,
-    output       update
+    input            S, I,
+    input      [1:0] mode,
+    input      [3:0] op_code,
+    output reg [3:0] EX_command,
+    output reg       mem_read,
+    output reg       mem_write,
+    output reg       WB_en,
+    output reg       Imm,
+    output reg       B,
+    output reg       update
 );
-    reg [3:0] EX_command_reg;
-    reg mem_read_reg, mem_write_reg,
-        WB_en_reg,
-        B_reg;
 
     always @(*) begin
-        mem_read_reg = 0;
-        mem_write_reg = 0;
-        WB_en_reg = 0;
-        B_reg = 0;
+        mem_read = 0;
+        mem_write = 0;
+        WB_en = 0;
+        B = 0;
         case (mode)
             `MODE_MEM: begin
                 case (S)
                     0: begin
-                        EX_command_reg = `EX_STR;
-                        mem_write_reg = 1;
+                        EX_command = `EX_STR;
+                        mem_write = 1;
                     end
 
                     1: begin
-                        EX_command_reg = `EX_LDR;
-                        mem_read_reg = 1;
-                        WB_en_reg = 1;
+                        EX_command = `EX_LDR;
+                        mem_read = 1;
+                        WB_en = 1;
                     end
                 endcase
             end
@@ -42,71 +38,63 @@ module Control_Unit
             `MODE_ARITHMETIC: begin
                 case (op_code)
                     `OP_MOV: begin
-                        EX_command_reg = `EX_MOV;
-                        WB_en_reg = 1;
+                        EX_command = `EX_MOV;
+                        WB_en = 1;
                     end
 
                     `OP_MVN: begin
-                        EX_command_reg = `EX_MVN;
-                        WB_en_reg = 1;
+                        EX_command = `EX_MVN;
+                        WB_en = 1;
                     end
 
                     `OP_ADD: begin
-                        EX_command_reg = `EX_ADD;
-                        WB_en_reg = 1;
+                        EX_command = `EX_ADD;
+                        WB_en = 1;
                     end
 
                     `OP_ADC: begin
-                        EX_command_reg = `EX_ADC;
-                        WB_en_reg = 1;
+                        EX_command = `EX_ADC;
+                        WB_en = 1;
                     end
 
                     `OP_SUB: begin
-                        EX_command_reg = `EX_SUB;
-                        WB_en_reg = 1;
+                        EX_command = `EX_SUB;
+                        WB_en = 1;
                     end
 
                     `OP_SBC: begin
-                        EX_command_reg = `EX_SBC;
-                        WB_en_reg = 1;
+                        EX_command = `EX_SBC;
+                        WB_en = 1;
                     end
                     `OP_AND: begin
-                        EX_command_reg = `EX_AND;
-                        WB_en_reg = 1;
+                        EX_command = `EX_AND;
+                        WB_en = 1;
                     end
 
                     `OP_ORR: begin
-                        EX_command_reg = `EX_ORR;
-                        WB_en_reg = 1;
+                        EX_command = `EX_ORR;
+                        WB_en = 1;
                     end
 
                     `OP_EOR: begin
-                        EX_command_reg = `EX_EOR;
-                        WB_en_reg = 1;
+                        EX_command = `EX_EOR;
+                        WB_en = 1;
                     end
 
                     `OP_CMP: begin
-                        EX_command_reg = `EX_CMP;
+                        EX_command = `EX_CMP;
                     end
 
                     `OP_TST: begin
-                        EX_command_reg = `EX_TST;
+                        EX_command = `EX_TST;
                     end
                 endcase
             end
 
             `MODE_BRANCH: begin
-                B_reg = 1;
+                B = 1;
             end
         endcase
     end
-
-    assign EX_command = EX_command_reg;
-    assign mem_read = mem_read_reg;
-    assign mem_write = mem_write_reg;
-    assign WB_en = WB_en_reg;
-    assign Imm = I;
-    assign B = B_reg;
-    assign update = S;
 
 endmodule
